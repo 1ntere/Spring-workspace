eec9b1af5d4d32128dc183abdff6c0e4c431c71d
1ntere <kwonhc9337java@gmail.com>
2024-06-18 17:39:09 +0900

    eclipse, spring 이용하여 DB와 연결해서 insert후 select로 불러오기

---
+++ demoProject3-mybatis-mapper-insert-select/src/main/java/com/shoppingmall/DemoProject3MybatisMapperInsertSelectApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperInsertSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperInsertSelectApplication.class, args);
+	}
+
+}
---
+++ demoProject3-mybatis-mapper-insert-select/src/test/java/com/shoppingmall/DemoProject3MybatisMapperInsertSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProject3MybatisMapperInsertSelectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
