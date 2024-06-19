e9ba7e50749d77129f8a90edd0cbf302497adc08
1ntere <kwonhc9337java@gmail.com>
2024-06-19 15:23:07 +0900

    DemoProject3-mybatis-mapper-select

---
+++ demoProject3-mybatis-mapper-select/src/main/java/com/shoppingmall/DemoProject3MybatisMapperSelectApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperSelectApplication.class, args);
+	}
+
+}
---
+++ demoProject3-mybatis-mapper-select/src/test/java/com/shoppingmall/DemoProject3MybatisMapperSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProject3MybatisMapperSelectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
