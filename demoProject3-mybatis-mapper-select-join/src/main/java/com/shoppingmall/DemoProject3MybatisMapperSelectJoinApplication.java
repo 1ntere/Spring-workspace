b28f71060ed218ce6030bc521da9ee3028f7a8be
1ntere <kwonhc9337java@gmail.com>
2024-06-20 11:47:38 +0900

    JOIN문을 활용해서 SELECT하기

---
+++ demoProject3-mybatis-mapper-select-join/src/main/java/com/shoppingmall/DemoProject3MybatisMapperSelectJoinApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProject3MybatisMapperSelectJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProject3MybatisMapperSelectJoinApplication.class, args);
+	}
+
+}
