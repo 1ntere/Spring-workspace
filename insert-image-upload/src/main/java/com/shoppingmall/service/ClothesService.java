2a5482dc49a93038dc3a6f5947c2058a81ae4151
1ntere <kwonhc9337java@gmail.com>
2024-06-21 17:35:03 +0900

    image 업로드하고 DB 저장 및 불러오기

--- insert-image-upload/src/main/java/com/shoppingmall/service/ClothesService.java
+++ insert-image-upload/src/main/java/com/shoppingmall/service/ClothesService.java
@@ -1,5 +1,7 @@
 package com.shoppingmall.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +24,7 @@ public class ClothesService {
 		return clothesMapper.getClothById(clothes_id);
 	}
 	
-	public void uploadCloth(String clothes_id,
+	public void uploadCloth(String clothes_name,
 			int clothes_price, 
 			String clothes_category, 
 			MultipartFile file) {
@@ -30,6 +32,23 @@ public class ClothesService {
 		System.out.println("file Name : " + fileName);
 		
 		String uploadDir = "C:/Users/user1/servlet_jsp_workspace/insert-image-upload/src/main/resources/static/images/";
+		File imgFile = new File(uploadDir + fileName);
+		if (!imgFile.exists()) {
+			imgFile.mkdirs();
+		}
+		
+		try {
+			file.transferTo(imgFile);
+			
+			Cloth cloth = new Cloth();
+			cloth.setClothes_name(clothes_name);
+			cloth.setClothes_price(clothes_price);
+			cloth.setClothes_category(clothes_category);
+			cloth.setClothes_image_path("/images/"+clothes_category+"/"+fileName);
+			clothesMapper.uploadCloth(cloth);
+			System.out.println("파일 업로드 Service를 성공적으로 완료했습니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
