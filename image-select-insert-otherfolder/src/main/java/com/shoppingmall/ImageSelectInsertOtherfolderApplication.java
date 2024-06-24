eb7d5df9a6b534ea03ce01ac4878fb477b7bab16
1ntere <kwonhc9337java@gmail.com>
2024-06-24 12:20:24 +0900

    이미지 보기, 상세페이지로 이동하기, 이미지 업로드

---
+++ image-select-insert-otherfolder/src/main/java/com/shoppingmall/ImageSelectInsertOtherfolderApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSelectInsertOtherfolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSelectInsertOtherfolderApplication.class, args);
+	}
+
+}
---
+++ image-select-insert-otherfolder/src/test/java/com/shoppingmall/ImageSelectInsertOtherfolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSelectInsertOtherfolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
