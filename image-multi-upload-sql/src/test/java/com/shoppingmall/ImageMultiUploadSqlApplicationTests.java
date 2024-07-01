b4a7d346be5ed430eef4e75632c273394cd2e652
1ntere <kwonhc9337java@gmail.com>
2024-07-01 13:37:25 +0900

    이미지 멀티 업로드, SELECT(포스트맨, ORACLE 사용)

---
+++ image-multi-upload-sql/src/test/java/com/shoppingmall/ImageMultiUploadSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageMultiUploadSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
