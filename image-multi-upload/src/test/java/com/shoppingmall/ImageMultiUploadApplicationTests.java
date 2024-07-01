64706c9d0adc5ca4f6e14d8a57da976f1a9639af
1ntere <kwonhc9337java@gmail.com>
2024-07-01 11:15:08 +0900

    이미지 멀티 업로드(포스트맨 사용)

---
+++ image-multi-upload/src/test/java/com/shoppingmall/ImageMultiUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageMultiUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
